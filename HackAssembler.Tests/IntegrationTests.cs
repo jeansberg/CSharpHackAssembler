aa8259c554413e1bf5f5c729e069ef609dd07e36
jeansberg <jeansberg@gmail.com>
2017-01-30 22:49:55 +0200

    Adds test project with integration tests and test files

---
+++ HackAssembler.Tests/IntegrationTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace HackAssembler.Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        Assembler testAssembler;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            testAssembler = new Assembler(new BasicParser(), new BasicCodeGenerator());
+        }
+
+        [TestMethod]
+        public void AddMatchesCompareFile()
+        {
+            CompareOutput("add/Add.asm");
+        }
+
+        [TestMethod]
+        public void MaxMatchesCompareFile()
+        {
+            CompareOutput("max/Max.asm");
+        }
+
+        [TestMethod]
+        public void RectMatchesCompareFile()
+        {
+            CompareOutput("rect/Rect.asm");
+        }
+
+        [TestMethod]
+        public void PongMatchesCompareFile()
+        {
+            CompareOutput("pong/Pong.asm");
+        }
+
+        private void CompareOutput(string programPath)
+        {
+            var inputFile = File.ReadAllText(programPath);
+
+            var programFolder = Path.GetDirectoryName(programPath);
+
+            var programName = Path.GetFileNameWithoutExtension(programPath);
+
+            testAssembler.Assemble(programName, inputFile);
+
+            var outputData = File.ReadAllText(Path.ChangeExtension(programPath, "hack"));
+
+            var comparisonData = File.ReadAllText(Path.ChangeExtension(programPath, "cmp"));
+
+            Assert.AreEqual(comparisonData, outputData);
+        }
+    }
+}
